28c7979951cdb6580873412b1ced897138ae0eb2
realMRFOX <83708770+realMRFOX@users.noreply.github.com>
2021-05-12 22:27:10 +0300

    Update MrTiger.js

--- MrTiger.js
+++ MrTiger.js
@@ -7387,8 +7387,8 @@ message.member.voiceChannel
 
 //تۆكینه‌كان لێره‌ جا بنێ
 
-client.login("ODM1Njg5MjUwODI5NjMxNDg5.YIeupQ.godag4QNNo2hd4btc1i-102XPp4");
-client2.login("");
+client.login("ODQxNzg5ODMzNTc5NzI0ODIw.YJsGOA.tZXktJ57_6S9r2pi9Xq6MkLjt_M");
+client2.login("ODQxNzg3ODcwMDQxMDc5ODA5.YJr9WA.Es4k621XinU9ybbQLDd59llSftY");
 client3.login("");
 client4.login("");
 client5.login("");
